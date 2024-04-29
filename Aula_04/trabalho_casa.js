7babcc8f6715e69ec60704ed500e5e0f3046bbad
Hugo Paes <34751586+HugoPaes@users.noreply.github.com>
2024-04-29 20:17:38 -0300

    Aula_04

---
+++ Aula_04/trabalho_casa.js
@@ -0,0 +1,13 @@
+// 2 - Analise o código utilizado na aula e descreva linha por linha seu funcionamento.
+// Obs: ele está comentado você pode apagar as // ou para agilizar selecionar o texto inteiro da linha 10 até 18 e utilizar o atalho CTRL + ; no Visual Studio code.
+
+function bisavo(){ //Função chamada "bisavo".
+     console.log("Eu sou o Bisavô"); // Exibe uma mensagem "Eu sou o Bisavô" na console do Terminal.
+     function avo(){ // Função chamada "avo".
+         console.log("Eu sou o Avô"); // Exibe uma mensagem "Eu sou o Avô" na console do Terminal.
+     }
+     avo(); // Exibe função.
+}
+
+ bisavo(); // Exibe função.
+
---
+++ Aula_04/verificar_arquivo.js
@@ -0,0 +1,12 @@
+// 1 - Utilizando o operador "typeof" imprima os tipos das constantes abaixo.
+// Ex:
+// const primeiroNome = "Ciclano"; 
+// console.log(typeof primeiroNome);
+// Number: Ex: 10, 5, 5.5, 11.22.
+// String: Ex: "Leo", "Lucas", "Fulano", "O valor é de R$".
+// Boolean: True é igual Verdadeiro e False é igual a Falso, Portanto verdadeiro ou falso.
+
+const hugo = "Paes";
+const pi = 3.14;
+const isNublado = false
+console.log(typeof isNublado);
