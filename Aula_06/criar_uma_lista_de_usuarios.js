2ed44b7b326317b87dec734e365966fb37a412af
Hugo Paes <34751586+HugoPaes@users.noreply.github.com>
2024-04-29 19:38:11 -0300

    algoritmos adicionando criar uma lista de usuarios
    algoritmos adicionando criar_uma_lista_de_usuarios.js

---
+++ Aula_06/criar_uma_lista_de_usuarios.js
@@ -0,0 +1,14 @@
+// 1 - Necessito criar um lista de usuários com os nomes
+// Sofia
+// Lucas
+// Isabella
+// Mateus
+// Alice
+// 
+// Essa lista deve ser armazenada em uma constante.
+// Quero que na tela seja impresso na saída os nomes, Sofia, Isabella e Alice.
+// Dica: Será necessário utilizar um recurso chamado ARRAY
+const nome = ["Sofia", "Lucas", "Isabella", "Mateus","Alice"];
+    console.log(nome[0]);
+    console.log(nome[2]);
+    console.log(nome[4]);
