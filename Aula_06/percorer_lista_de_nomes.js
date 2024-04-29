69f9fc6e225bf50158882fb918d201a4e40c5ac5
Hugo Paes <34751586+HugoPaes@users.noreply.github.com>
2024-04-29 20:12:33 -0300

    feat adicionando percorrer lista de nomes
    feat adicionando percorrer_lista_de_nomes.js

---
+++ Aula_06/percorer_lista_de_nomes.js
@@ -0,0 +1,8 @@
+// 1 - Utilizando ForLoop e array juntos, faça uma lista com 5 nomes, e faça o forLoop percorrer por ela exibindo todos os nomes um por um.
+// Será necessário um método de array chamado "length".
+const nomes = ['maria', 'josé', 'joão', 'fernando', 'pedro'];
+
+for(let i = 0; i < nomes.length; i++) {
+    console.log(nomes[i]);
+
+}
